462b3ecbd977a33f70b4b33a774398eb8acb4aea
AHYEON04 <201120cay@dsm.hs.kr>
2021-01-07 22:23:48 +0900

    Create index.ts

---
+++ Typechain/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi("ahyeon", 18, "female");
+
+export {};
