bb1b26e6470f9d15c92cde905ae3833067ff9543
AHYEON04 <201120cay@dsm.hs.kr>
2021-01-07 23:02:43 +0900

    Update index.ts

--- Typechain/index.ts
+++ Typechain/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string) => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "ahyeon",
+  age: 18,
+  gender: "female",
 };
 
-sayHi("ahyeon", 18, "female");
+const sayHi = (person: Human): string => {
+  return `Hello ${name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 export {};
