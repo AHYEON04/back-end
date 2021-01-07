9fa3167c4e170bc6e01428def81a1fc9c824e07a
AHYEON04 <201120cay@dsm.hs.kr>
2021-01-07 23:38:27 +0900

    Update index.ts

--- Typechain/index.ts
+++ Typechain/index.ts
@@ -1,19 +1,20 @@
-interface Human {
+class Human {
   name: string;
   age: number;
   gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "ahyeon",
-  age: 18,
-  gender: "female",
-};
+const lynn = new Human("Lynn", 18, "female");
 
 const sayHi = (person: Human): string => {
   return `Hello ${name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
