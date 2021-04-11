0bc84e8059b06ca2bac6583074c90243f0c47653
최아연 <201120cay@dsm.hs.kr>
2021-04-11 20:30:11 +0900

    Update index.ts

--- Typechain/index.ts
+++ Typechain/index.ts
@@ -40,33 +40,5 @@ const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
 let blockchain: [Block] = [genesisBlock];
 
-const getBlockchain = (): Block[] => blockchain;
-const getLatesBlock = (): Block => blockchain[blockchain.length - 1];
-const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
-const createNewBlock = (data: string): Block => {
-  const priviosBlock: Block = getLatesBlock();
-  const newIndex: number = previosBlock.index + 1;
-  const nextTimestamp: number = getNewTimeStamp();
-  const nextHash: string = Block.calculateBlockHash(
-    newIndex,
-    previosBlcok.hash,
-    nextTimestamp,
-    data
-  );
-  const newBLOCK: Block = new Block(
-    newIndex,
-    newHash,
-    previosBlock.hash,
-    data,
-    newTimestamp
-  );
-  return newBLOCK;
-};
-
-const isBlockValid = (candidateBlock: Block, previosBlock: Block): boolean => {
-  if (Block.validateStructure(candidateBlock)) {
-    return false;
-  }
-};
 
 export {};
