67587cbf1ab4eb760ea2bd0f55b80a14cc48a357
최아연 <201120cay@dsm.hs.kr>
2021-04-14 09:35:09 +0900

    Update index.ts

--- Typechain/index.ts
+++ Typechain/index.ts
@@ -1,4 +1,4 @@
-import * as CryptoJS from "crypto-js";
+import * as CryptoJs from "crypto-js";
 
 class Block {
   public index: number;
@@ -6,21 +6,15 @@ class Block {
   public previousHash: string;
   public data: string;
   public timestamp: number;
-
+  
   static calculateBlockHash = (
-    index: number,
-    previousHash: string,
-    timestamp: number,
+    index:number,
+    previousHash: string, 
+    timestamp: number, 
     data: string
-  ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toSr;
-
-  static validateStructure = (aBlock: Block): boolean =>
-    typeof aBlock.index === "number" &&
-    typeof aBlock.hash === "string" &&
-    typeof aBlock.previousHash === "string" &&
-    typeof aBlock.timestamp === "number" &&
-    typeof aBlock.data === "string";
-
+    ) :string => 
+    CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+    
   constructor(
     index: number,
     hash: string,
@@ -36,9 +30,16 @@ class Block {
   }
 }
 
+
+
 const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
 let blockchain: [Block] = [genesisBlock];
 
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime()/1000);
 
 export {};
