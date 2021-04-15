ae77a316bcfd7da5a7ccbe1a12c54160d773f68b
최아연 <201120cay@dsm.hs.kr>
2021-04-15 19:09:23 +0900

    Update index.ts

--- Typechain/index.ts
+++ Typechain/index.ts
@@ -34,12 +34,18 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length -1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime()/1000);
+
+const createNewBlock = (data: string): Block => {
+  const previosBlock :  Block = getLatestBlock();
+  const newIndex : number = previosBlock.index + 1;
+  const nextTimestamp: number = getNewTimeStamp();
+};
 
 export {};
