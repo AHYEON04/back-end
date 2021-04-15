889e4addbb10b332e3cd3fe28a06136cd8db8bed
최아연 <201120cay@dsm.hs.kr>
2021-04-15 19:28:25 +0900

    Update index.js

--- Typechain/index.js
+++ Typechain/index.js
@@ -1,14 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-  constructor(name, age, gender) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+const CryptoJS = require("crypto-js");
+class Block {
+  constructor(index, hash, previousHash, data, timestamp) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
-const lynn = new Humman("Lynn", 18, "female");
-const sayHi = (person) => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+const genesisBlock = new Block(0, "2020202020202","","Hello", 123456);
+let blockchain = [genesisBlock];
+const getBlockchain = ()=> blockchain;
+const getLatestBlock = ()=> blockchain[blockchian.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime()/1000);
+const createNewBlock = (data)=>{
+  const previousBlock = getLatestBlock();
+  const newIndex = previousBlock.index + 1;
+  const nextTimestamp = getNewTimeStamp();
 };
-console.log(sayHi(lynn));
